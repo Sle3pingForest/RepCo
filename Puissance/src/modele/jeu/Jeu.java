615d0d6b8f347ae3b5321d8d0a95b4cd26d9afff
Hue Nam <huenam.ly@gmail.com>
2018-02-14 17:37:39 +0100

    update

--- Puissance/src/modele/jeu/Jeu.java
+++ Puissance/src/modele/jeu/Jeu.java
@@ -23,6 +23,42 @@ public class Jeu {
 			}
 		}
 	}
+
+
+
+	public Joueur getJ1() {
+		return j1;
+	}
+
+
+
+	public void setJ1(Joueur j1) {
+		this.j1 = j1;
+	}
+
+
+
+	public Joueur getJ2() {
+		return j2;
+	}
+
+
+
+	public void setJ2(Joueur j2) {
+		this.j2 = j2;
+	}
+
+
+
+	public int[][] getPlateau() {
+		return plateau;
+	}
+
+
+
+	public void setPlateau(int[][] plateau) {
+		this.plateau = plateau;
+	}
 	
 	
 
